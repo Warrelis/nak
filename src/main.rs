48c5490a3e93306b18b5f2d274e731512cb064ad
Joshua Warner <joshuawarner32@gmail.com>
2018-06-27 19:48:47 -0700

    Don't exit on the first sign of trouble

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#[macro_use]
+extern crate failure;
+
 use std::io::Write;
 use std::io;
 use std::process as pr;
+
+use failure::Error;
 
 pub enum Tool {
     Path(String),
@@ -21,31 +26,30 @@ pub enum Response {
 }
 
 pub trait Remote {
-    fn run(&self, c: Command) -> Response;
+    fn run(&self, c: Command) -> Result<Response, Error>;
 }
 
 pub struct SimpleRemote;
 
 impl Remote for SimpleRemote {
-    fn run(&self, c: Command) -> Response {
+    fn run(&self, c: Command) -> Result<Response, Error> {
         match c.tool {
             Tool::Path(path) => {
                 let out = pr::Command::new(path)
                     .args(&c.args.args)
-                    .output()
-                    .expect("failed to execute process");
+                    .output()?;
 
-                Response::Text(String::from_utf8(out.stdout).unwrap())
+                Ok(Response::Text(String::from_utf8(out.stdout)?))
             }
         }
     }
 }
 
 trait Reader {
-    fn get_command(&mut self, remote: &dyn Remote) -> Command;
+    fn get_command(&mut self, remote: &dyn Remote) -> Result<Command, Error>;
 }
 
-fn parse_command_simple(input: &str) -> Command {
+fn parse_command_simple(input: &str) -> Result<Command, Error> {
     let parts = input.trim().split(" ");
     let mut c = None;
     let mut args = Vec::new();
@@ -56,24 +60,23 @@ fn parse_command_simple(input: &str) -> Command {
             args.push(p.to_string());
         }
     }
-    Command {
-        tool: Tool::Path(c.unwrap()),
+    Ok(Command {
+        tool: Tool::Path(c.ok_or_else(|| format_err!("No binary"))?),
         args: Args { args },
-    }
+    })
 }
 
 struct SimpleReader;
 
 impl Reader for SimpleReader {
-    fn get_command(&mut self, _: &dyn Remote) -> Command {
+    fn get_command(&mut self, _: &dyn Remote) -> Result<Command, Error> {
         let mut input = String::new();
 
         print!("> ");
         io::stdout().flush().unwrap();
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => parse_command_simple(&input),
-            Err(error) => panic!("error: {}", error),
-        }
+        Ok(io::stdin().read_line(&mut input)
+            .map_err(Error::from)
+            .and_then(|_| parse_command_simple(&input))?)
     }
 }
 
@@ -82,12 +85,14 @@ fn main() {
     let remote = SimpleRemote;
 
     loop {
-        let cmd = reader.get_command(&remote);
+        let e = reader.get_command(&remote)
+            .and_then(|cmd| remote.run(cmd))
+            .map(|res| match res {
+                Response::Text(text) => println!("{}", text),
+            }).err();
 
-        let res = remote.run(cmd);
-
-        match res {
-            Response::Text(text) => println!("{}", text),
+        if let Some(e) = e {
+            println!("{:?}", e);
         }
     }
 }
