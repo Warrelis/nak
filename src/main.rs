467c5d47f8217686e7d02cb502eb36c5695a94a0
Joshua Warner <joshuawarner32@gmail.com>
2018-07-11 22:49:16 -0700

    Resurrect SimpleRemote

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ extern crate failure;
 extern crate os_pipe;
 extern crate futures;
 extern crate tokio;
-extern crate rand;
 extern crate base64;
 
 use std::borrow::Borrow;
-use rand::thread_rng;
-use rand::Rng;
 use os_pipe::IntoStdio;
 use std::io::Read;
 use std::io::Write;
@@ -52,134 +49,57 @@ pub enum Response {
     Remote(Box<dyn Remote>),
 }
 
+struct SingleStream {
+    out: Option<String>
+}
+
+impl Stream for SingleStream {
+    type Item = String;
+    type Error = Error;
+
+    fn poll_next(
+        &mut self,
+        _cx: &mut Context
+    ) -> Result<Async<Option<Self::Item>>, Self::Error> {
+        match self.out.take() {
+            Some(out) => Ok(Async::Ready(Some(out))),
+            None => Ok(Async::Ready(None))
+        }
+    }
+}
+
 pub trait Remote {
     fn run(&mut self, c: Command) -> Result<Response, Error>;
 }
 
-struct ShRemote<I: Write, O: Read, E: Read> {
-    input: I,
-    output: O,
-    error: E,
-}
+pub struct SimpleRemote;
 
-struct KeyedOutput<R: Read> {
-    key: Vec<u8>,
-    carryover_buf: Option<Vec<u8>>,
-    match_pos: usize,
-    inner: R,
-}
-
-impl<R: Read> KeyedOutput<R> {
-    fn new(key: Vec<u8>, inner: R) -> KeyedOutput<R> {
-        KeyedOutput {
-            key,
-            carryover_buf: None,
-            match_pos: 0,
-            inner,
-        }
-    }
-
-    fn at_end(&self) -> bool {
-        self.carryover_buf.is_some()
-    }
-}
-
-impl<R: Read> Read for KeyedOutput<R> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        assert!(buf.len() >= self.key.len()); // TODO: handle this gracefully
-
-        assert!(self.carryover_buf.is_none()); // This would mean we're at eof; todo: handle gracefully?
-
-        let len = self.inner.read(buf)?;
-        let buf = &mut buf[..len];
-
-        if len+self.match_pos >= self.key.len() {
-            for i in 0 .. len+self.match_pos-self.key.len() {
-                if &buf[i .. i+self.key.len()-self.match_pos] == &self.key[self.match_pos..] {
-                    self.carryover_buf = Some(buf[len-self.key.len()+self.match_pos .. len].to_vec());
-                    return Ok(i);
-                }
-                self.match_pos = 0;
-            }
-        } else {
-            for i in 0 .. len {
-                if &buf[i..] == &self.key[i..] {
-                    self.match_pos = i;
-                    return Ok(0);
-                }
-            }
-        }
-
-        Ok(len)
-    }
-}
-
-fn make_key() -> String {
-    let mut bytes = [0u8; 16];
-    thread_rng().fill(&mut bytes);
-    base64::encode(&bytes)
-}
-
-fn spawn_sh_remote() -> Result<ShRemote<os_pipe::PipeWriter, os_pipe::PipeReader, os_pipe::PipeReader>, Error> {
-    let mut cmd = pr::Command::new("sh");
-
-    let (output_reader, output_writer) = os_pipe::pipe()?;
-    let (error_reader, error_writer) = os_pipe::pipe()?;
-    let (input_reader, input_writer) = os_pipe::pipe()?;
-
-    cmd.stdout(output_writer.into_stdio());
-    cmd.stderr(error_writer.into_stdio());
-    cmd.stdin(input_reader.into_stdio());
-
-    let _child = cmd.spawn()?;
-
-    drop(cmd);
-
-    Ok(ShRemote {
-        input: input_writer,
-        output: output_reader,
-        error: error_reader,
-    })
-}
-
-impl<I: Write, O: Read, E: Read> Remote for ShRemote<I, O, E> {
-    fn run(&mut self, c: Command) -> Result<Response, Error> {
-        let args = c.args.args.join(" "); // TODO: proper escaping!
+impl Remote for SimpleRemote {
+    fn run(&mut self, c: Command) -> Result<Response,
+ Error> {
         match c.tool {
             Tool::Path(path) => {
-                let key = make_key();
-                let line = format!("{} {}; echo {}${{?}}e\n", path, args, key);
-                // println!("writing {:?}", line);
-                self.input.write(line.as_bytes())?;
-                self.input.flush()?;
+                let mut cmd = pr::Command::new(path);
+                cmd.args(&c.args.args);
 
-                let mut output = Vec::new();
+                let (mut reader, writer) = os_pipe::pipe()?;
 
-                let mut key_output = KeyedOutput::new(key.into_bytes(), &mut self.output);
+                cmd.stdout(writer.into_stdio());
 
-                loop {
-                    let mut buf = [0u8; 1024];
-                    let len = key_output.read(&mut buf)?;
-                    output.extend(&buf[..len]);
-                    if key_output.at_end() {
-                        break;
-                    }
-                }
+                let mut handle = cmd.spawn()?;
 
-                Ok(Response::Text(String::from_utf8(output)?))
-            },
-            Tool::Remote { host } => {
-                let key = make_key();
-                let key_err = make_key();
-                let line = format!("ssh {} sh; echo {}${{?}}e; echo {} >&2\n", args, key, key_err);
-                // println!("writing {:?}", line);
-                self.input.write(line.as_bytes())?;
-                self.input.flush()?;
+                drop(cmd);
 
-                // TODO: non-blocking...
+                let mut output = String::new();
 
-                panic!();
+                reader.read_to_string(&mut output)?;
+                handle.wait()?;
+
+                Ok(Response::Stream {
+                    stdout: Box::new(SingleStream { out: Some(output) })
+                })
             }
+            Tool::Remote { .. } => panic!(),
         }
     }
 }
@@ -280,8 +200,7 @@ fn remote_run(mut remote: Box<dyn Remote>, reader: &mut dyn Reader)
 
 fn main() {
     let mut reader = SimpleReader;
-    // let mut remote = SimpleRemote;
-    let remote = Box::new(spawn_sh_remote().unwrap());
+    let remote = Box::new(SimpleRemote);
 
     remote_run(remote, &mut reader).unwrap();
 }
